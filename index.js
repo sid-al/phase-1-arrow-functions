a0816896b9edfca153db3f630662b9acf2d03e0e
sid-al <sendplusx@gmail.com>
2022-06-07 22:58:10 +0300

    commit tree 0674f743a5e834c70cca4d072c4b25ea0e339bb5

---
+++ index.js
@@ -0,0 +1,36 @@
+// function declaration
+
+// function foo (){
+//     return 'bar';
+// }
+
+// // function expression
+
+// const foo = function (){
+//     return 'bar';
+// }
+
+// Arrow function
+
+// const add = (a, b) => a + b;
+// console.log(add(2,3)); // = 5
+
+// const sum = (num1, num2) => {
+//     console.log(`Adding ${num1}`);
+//     console.log(`Adding ${num2}`);
+//     return num1 + num2;
+// }
+
+// console.log(sum (1,2));
+
+
+// const nums = [24,65,95,20];
+// const squares = nums.map( x => x ** 2);
+// console.log(squares());
+// console.log(nums());
+
+
+
+let divide = (a, b) => 2000/100;
+let square = x => x * x;
+let add = (a, b) => a + b;
